a0e4269239fa8dec68c18935aaf8237b0d3eabc5
yraja963 <yraja@gmail.com>
2021-01-20 11:45:05 +0530

    answer2

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get("/topRankings", async (req,res)=>{
+    const offset=getValue(req.query.offset,0);
+    const limit=getValue(req.query.limit,20) +offset;
+    const dataToSend=await data.slice(offset,limit);
+    console.log(dataToSend);
+    res.send(dataToSend);
+});
+
+const getValue = (value, defaultValue) => {
+    if (value === null || value === undefined || isNaN(Number(value))) {
+      return defaultValue;
+    }
+    return Number(value);
+  };
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
